60f9e7a475ca2e8186b895a71afe951971aac726
aeciobrito@ideapad <aeciobrito@gmail.com>
2023-06-04 15:25:11 -0300

    Passing data for the card

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -7,17 +7,31 @@ import { OpenWeatherMapService } from '../open-weather-map.service';
   styleUrls: ['./weather-card.component.scss']
 })
 export class WeatherCardComponent implements OnInit {
-  temperature = "50 c";
-  fellsLike = "55 c";
-  pressure = "500 c";
-  humidity = "20% c";
+
+  temp: number = 0;
+  feels_like: number = 0;
+  temp_min: number = 0;
+  temp_max: number = 0;
+  pressure: number = 0;
+  humidity: number = 0;
+
+  icon: string = "";
+
+  myWeather: any;
 
   constructor(private weatherService: OpenWeatherMapService) {}
 
   ngOnInit(): void {
     this.weatherService.getWeather().subscribe({
       next: (res) => {
-        console.log(res)
+        this.myWeather = res;
+        this.temp = this.myWeather.main.temp;
+        this.feels_like = this.myWeather.main.feels_like;
+        this.temp_min = this.myWeather.main.temp_min;
+        this.temp_max = this.myWeather.main.temp_max;
+        this.pressure = this.myWeather.main.pressure;
+        this.humidity = this.myWeather.main.humidity;
+        this.icon = 'https://openweathermap.org/img/wn/' + this.myWeather.weather[0].icon + '@2x.png'
       },
       error: (error) => console.log(error.message),
       complete: () => console.info('API call completed')
