27d120e05481b503331e49fb322985fe7f28cea5
aeciobrito@ideapad <aeciobrito@gmail.com>
2023-06-10 20:51:25 -0300

    debounce time input search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OpenWeatherMapService } from './shared/services/open-weather-map.service';
 import { Forecast } from './shared/models/Forecast';
+import { debounceTime, filter } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-weatherapp';
   cidade!: string;
+  cidadeInput$ = new Subject<string>();
+
 
   myWeather: Forecast | undefined;
 
   constructor(private weatherService: OpenWeatherMapService) {}
 
+  ngOnInit(): void {
+    this.cidadeInput$.pipe(
+      debounceTime(1000),
+      filter(cidade => cidade.trim() !== '')
+    ).subscribe(() => {
+      this.getWeatherData();
+    });
+  }
+
   getWeatherData(): void {
-    this.weatherService.getWeather(this.cidade).subscribe({
+    this.weatherService.getWeather(this.cidade.trim()).subscribe({
       next: (res) => {
         this.myWeather = new Forecast(res);
       },
